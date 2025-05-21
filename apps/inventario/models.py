ab71095f33d1e33543c27139efc99b6e53ef8ead
jeankaled <je.avilaa@duocuc.cl>
2025-05-20 22:13:53 -0400

    se agrega models en apss

--- apps/inventario/models.py
+++ apps/inventario/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class inventario(models.Model):
+    fecha_ingreso = models.DateTimeField(auto_now=True)
+    cantidad_disponible = models.PositiveIntegerField()
+    producto = models.ForeignKey('productos.producto', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.producto} - {self.inventario.id}"
+    
+
